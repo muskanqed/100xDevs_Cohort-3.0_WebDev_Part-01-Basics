0c72a00a6301db78f8023acea5355fe16ca3818f
muskanqed <muskanqed@gmail.com>
2024-09-12 23:49:17 +0530

    Completed DOM advance session

---
+++ Week-03_DOM/3.2-DOM(Advance)/LectureCode/scriptReact.js
@@ -0,0 +1,44 @@
+let todos = [];
+
+function addTodo() {
+  todos.push({
+    title: document.querySelector("input").value,
+  });
+  render();
+}
+
+function deleteLastTodo() {
+  todos.splice(todos.length - 1, 1); // remove the last element from the arr
+  render();
+}
+
+function deleteFirstTodo() {
+  todos.splice(0, 1); // remove the last element from the arr
+  render();
+}
+
+function deleteTodoAt(index) {
+  todos.pop();
+  render();
+}
+
+function createTodoComponent(todo, index) {
+  const div = document.createElement("div");
+  const h1 = document.createElement("h1");
+  const button = document.createElement("button");
+  button.innerHTML = "Delete";
+  button.setAttribute("onclick", "deleteTodoAt(" + index + ")");
+  h1.innerHTML = todo.title;
+  div.append(h1);
+  div.append(button);
+  return div;
+}
+
+// react
+function render() {
+  document.querySelector("#todos").innerHTML = "";
+  for (let i = 0; i < todos.length; i++) {
+    const element = createTodoComponent(todos[i], i);
+    document.querySelector("#todos").appendChild(element);
+  }
+}
---
+++ Week-03_DOM/3.2-DOM(Advance)/Notes.md
@@ -0,0 +1,3 @@
+# **Week 03 - 3.2 | DOM(Advance)**
+
+## [Notes / Slides Link](https://projects.100xdevs.com/tracks/dom-2/DOM-Part-2--1)
