d7e6734020293bee88a7468f8d39dee22526242a
muskanqed <muskanqed@gmail.com>
2024-11-07 11:56:21 +0530

    CounterApp using React

---
+++ Week-09_React/9.1-React-Basics/counterapp/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  const [count, setCount] = React.useState(0);
+
+  function onClickHandler() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <button onClick={onClickHandler}>Counter {count}</button>
+    </div>
+  )
+}
+export default App
