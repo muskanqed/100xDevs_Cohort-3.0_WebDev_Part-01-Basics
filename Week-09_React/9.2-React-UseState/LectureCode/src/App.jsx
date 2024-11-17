74b9835bed489244baa499dbed0ee54171d468a0
muskanqed <muskanqed@gmail.com>
2024-11-17 22:29:41 +0530

    Usestate,UseEffect and conditional rendering

--- Week-09_React/9.2-React-UseState/LectureCode/src/App.jsx
+++ Week-09_React/9.2-React-UseState/LectureCode/src/App.jsx
@@ -1,36 +1,52 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  // Condition rendering: if true then render else dont render
+  const [counterVisible, setCounterVisible] = useState(true);
+
+  useEffect(() => {
+    setInterval(() => {
+      setCounterVisible(c => !c)
+    }, 5000)
+  }, [])
+
   return (
     <>
       <div>
-        <Counter></Counter>
+        {counterVisible && <Counter></Counter>}
       </div>
     </>
   );
 }
 
+// Mounting, Re-rendering and unmounting
 function Counter() {
   const [count, setCount] = useState(0);
 
-  function increaseCount() {
-    setCount(count + 1);
-  }
-  function decreaseCount(){
-    setCount(count-1);
-  }
+  // Check this log
+  console.log("Re-rendering");
 
-  function resetCount(){
-    setCount(0);
-  }
+
+  // useEffect hooks ensures that our element is hooked once when it mounts and does change even if re-rendering happens
+
+  useEffect(() => {
+    setInterval(() => {
+      setCount(count => count + 1)
+    }, 1000)
+    console.log("Mounted once");
+
+  }, [])
+
+  // function increaseCount() {
+  //   setCount(count + 1);
+  // }
+
 
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={increaseCount}>Increase Count</button>
-      <button onClick={decreaseCount}>Decrease Count</button>
-      <button onClick={resetCount}>Reset</button>
+      {/* <button onClick={increaseCount}>Increase Count</button> */}
     </div>
   );
 }
